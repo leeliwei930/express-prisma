b982ab0c67b85a0fbe496bc19e4a4a2218397ce1
Lee Li Wei <leeliwei930@gmail.com>
2024-09-21 17:40:19 +0800

    include sample of file upload

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,11 +1,53 @@
 import { Router } from "express";
+import multer from "multer";
 
 const router = Router();
 
+const storage = multer.memoryStorage();
+
+// 20MB maximum file upload size
+const MAX_FILE_SIZE = 20_000_000;
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: MAX_FILE_SIZE },
+});
+
 router.get("/health", (req, res, _) => {
+  let reqBody = req.body;
+
   res.json({
     status: "OK",
   });
 });
+
+
+// Sample Code for handlings file upload
+type MultiUploadFiles = {
+    [fieldname: string]: Express.Multer.File[]
+}
+
+router.post(
+  "/upload",
+  upload.fields([{ name: "verification_doc", maxCount: 1 }]),
+  (req, res, _) => {
+    let files = req.files as MultiUploadFiles;
+    if (files?.length) {
+      res.status(400).send("No files uploaded.");
+      return;
+    }
+
+    let vDoc = files["verification_doc"]?.[0];
+    if (!vDoc) {
+      res.status(400).send("No file uploaded.");
+      return;
+    }
+
+    res.json({
+      status: "OK",
+      file: vDoc.originalname,
+    });
+  }
+);
 
 export default router;
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,14 @@ import express from "express";
 import apiRoutes from "../routes/api";
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/api/v1", apiRoutes);
 
 type StartParams = { address: string; port: number };
 
-const start = function ({ address, port }: StartParams) {
+const start = function({ address, port }: StartParams) {
   console.log(`⏳ Starting up server`);
   app.listen(port, address, () => {
     console.log(`🚀 Server listen on port http://${address}:${port}`);
